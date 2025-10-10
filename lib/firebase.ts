86c0d5bc5ca155a6bffa6a4e4a3ae9a860565b65
moathssnck <myneww00@gmail.com>
2025-10-10 13:16:58 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBPPr07SVQxQ6IOR_7sU0D1Vm48UZb_zcM",
+  authDomain: "zxsz-33a85.firebaseapp.com",
+  databaseURL: "https://zxsz-33a85-default-rtdb.firebaseio.com",
+  projectId: "zxsz-33a85",
+  storageBucket: "zxsz-33a85.firebasestorage.app",
+  messagingSenderId: "330185565279",
+  appId: "1:330185565279:web:d037f8511a5421924667ea",
+  measurementId: "G-08HW2LNF9K"
 };
 
 const app = initializeApp(firebaseConfig);
