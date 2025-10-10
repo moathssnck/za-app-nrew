d94608dca55829890f3a9e27c7e06b7787eca6de
moathssnck <myneww00@gmail.com>
2025-10-10 14:44:47 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // this tells Next.js to output a static site
 };
 
 export default nextConfig;
